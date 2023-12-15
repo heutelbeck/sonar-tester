9f6799d6b9f421c613e20a17c39ba30e2f7d1232
Dominic Heutelbeck <dominic.heutelbeck@gmail.com>
2023-12-15 09:50:04 +0100

    add some code

---
+++ src/main/java/com/example/sonartester/SonarTesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonartester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonartester/SonarTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonartester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
